39820facc262e981147cf28cd14ddcfae9b96b1f
SJHlll <ssaadd1212@gmail.com>
2024-05-23 16:15:53 +0900

    카카오 로그인, 로그아웃, 프로필 사진 처리

--- src/App.js
+++ src/App.js
@@ -3,28 +3,31 @@ import TodoTemplate from './components/todo/TodoTemplate';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header';
 import { Route, Routes } from 'react-router-dom';
-import Join from './components/user/Join';
 import Login from './components/user/Login';
+import Join from './components/user/Join';
 import { AuthContextProvider } from './utils/AuthContext';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import KakaoLoginHandler from './components/user/KakaoLoginHandler';
 
 function App() {
   return (
-    <>
-      <AuthContextProvider>
-        <div className="wrapper">
-          <Header />
-          <div className="content-wrapper">
-            <Routes>
-              <Route path="/" element={<TodoTemplate />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="/join" element={<Join />} />
-            </Routes>
-          </div>
-          <Footer />
+    // 데이터를 전달하고자 하는 자식 컴포넌트를 Provider로 감쌉니다.
+    <AuthContextProvider>
+      <div className="wrapper">
+        <Header />
+
+        <div className="content-wrapper">
+          <Routes>
+            <Route path="/" element={<TodoTemplate />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/join" element={<Join />} />
+            <Route path="/oauth/kakao" element={<KakaoLoginHandler />} />
+          </Routes>
         </div>
-      </AuthContextProvider>
-    </>
+
+        <Footer />
+      </div>
+    </AuthContextProvider>
   );
 }
 
